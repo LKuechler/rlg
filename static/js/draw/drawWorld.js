ec9d2e40c1921715424f11eabc81b03b4649f501
Lasse Küchler <laskue@sinnerschrader.com>
2014-12-11 00:22:26 +0100

    optimized the canvas drawing - only draw with color when there is no titleset for the block

--- static/js/draw/drawWorld.js
+++ static/js/draw/drawWorld.js
@@ -5,10 +5,11 @@ function drawWorld() {
 		for (var y = 0; y < world.height; y++)
 		{
 			// draw it
-			blockLayer.fillStyle = world[x][y].color;
-			blockLayer.fillRect(x*tileWidth, y*tileHeight, tileWidth, tileHeight);
 			if(world[x][y].img) {
 				blockLayer.drawImage(world[x][y].img, 0, 0, 16, 16, x*tileWidth, y*tileHeight, tileWidth, tileHeight);
+			} else {
+				blockLayer.fillStyle = world[x][y].color;
+				blockLayer.fillRect(x*tileWidth, y*tileHeight, tileWidth, tileHeight);
 			}
 		}
 	}
@@ -19,10 +20,11 @@ function drawWorld() {
 		for (var y = 0; y < world.height; y++)
 		{
 			// draw it
-			ambientLayer.fillStyle = ambient[x][y].color;
-			ambientLayer.fillRect(x*tileWidth, y*tileHeight, tileWidth, tileHeight);
 			if(ambient[x][y].img) {
 				ambientLayer.drawImage(ambient[x][y].img, 0, 0, 16, 16, x*tileWidth, y*tileHeight, tileWidth, tileHeight);
+			} else {
+				ambientLayer.fillStyle = ambient[x][y].color;
+				ambientLayer.fillRect(x*tileWidth, y*tileHeight, tileWidth, tileHeight);
 			}
 		}
 	}
