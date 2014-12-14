8973373213de20fe5e803bdd2f126ce3e962980e
Lasse Küchler <laskue@sinnerschrader.com>
2014-12-14 20:36:43 +0100

    untestet code for ai tasks

---
+++ static/js/ai/tasks.js
@@ -0,0 +1,34 @@
+function moveEntity(x, y, entity) {
+	if(world[x][y].passable === false){
+		return false;
+	}
+	entity.x = x;
+	entity.y = y;
+	return true;
+}
+
+
+function seek(x, y, entity) {
+	entity.tasks.shift(); //removes first task from entity
+
+	var xDiff = diff(x, entity.x);
+	var yDiff = diff(y, entity.y);
+	var response;
+	if(xDiff > yDiff) {
+		if(x > entity.x) {
+			response = moveEntity(entity.x-1, entity.y, entity);
+		} else {
+			response = moveEntity(entity.x+1, entity.y, entity);
+		}
+	} else {
+		if(y > entity.y) {
+			response = moveEntity(entity.x, entity.y-1, entity);
+		} else {
+			response = moveEntity(entity.x, entity.y+1, entity);
+		}
+	}
+
+	if(x != entity.x && y != entity.y && response) { //when entity is not yet at the desired location re add seek to tasks
+		entity.tasks.unshift(seek(x, y, entity)); //add task for entity at top of tasks list
+	}
+}
